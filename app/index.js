3897022f5c7a68cd9f38e126402897f222d49b53
Jordan Marcon <marcon.jordan@gmail.com>
2021-05-12 20:04:59 +0100

    Initial version

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import Mihawk from 'images/mihawk.jpg'
+
+console.log(Mihawk)
+
+const test = () => {
+  console.log('test')
+}
+
+test()
+
+window.setTimeout(() => {
+  console.log('test')
+}, 2000)
