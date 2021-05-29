6bf6f9811d523614404842999cb50b07e692612f
Jordan Marcon <marcon.jordan@gmail.com>
2021-05-29 14:11:21 +0100

    Setting up the JavaScript architecture of your application: done

--- app/index.js
+++ app/index.js
@@ -1,15 +1,73 @@
-import Mihawk from 'images/mihawk.jpg'
+import About from 'pages/About'
+import Collections from 'pages/Collections'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
 
-console.log(Mihawk)
+class App {
+  constructor () {
+    this.createContent()
+    this.createPages()
 
-const test = () => {
-  console.log('test')
+    this.addLinkListeners()
+  }
+
+  createContent () {
+    this.content = document.querySelector('.content')
+    this.template = this.content.getAttribute('data-template')
+  }
+
+  createPages () {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
+
+    this.page = this.pages[this.template]
+    this.page.create()
+    this.page.show()
+    // console.log(this.pages)
+  }
+
+  async onChange (url) {
+    await this.page.hide()
+    const request = await window.fetch(url)
+
+    if (request.status === 200) {
+      const html = await request.text()
+      const div = document.createElement('div')
+
+      div.innerHTML = html
+
+      const divContent = div.querySelector('.content')
+
+      this.template = divContent.getAttribute('data-template')
+
+      this.content.setAttribute('data-template', this.template)
+      this.content.innerHTML = divContent.innerHTML
+
+      this.page = this.pages[this.template]
+      this.page.create()
+      this.page.show()
+    } else {
+      console.log('error')
+    }
+  }
+
+  addLinkListeners () {
+    const links = document.querySelectorAll('a')
+
+    links.forEach(link => {
+      link.onclick = event => {
+        event.preventDefault()
+
+        const { href } = link
+
+        this.onChange(href)
+      }
+    })
+  }
 }
 
-test()
-
-window.setTimeout(() => {
-  console.log('test')
-}, 2000)
-
-console.log('test')
+new App()
